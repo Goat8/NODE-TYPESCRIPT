dab68be36a2e2b833c9bbe7a5524456ae5042c0e
Goat8 <mariarafique12@gmail.com>
2023-10-03 15:32:29 +0500

    Fix: move the common logic in function

--- src/services/sku/sku.service.ts
+++ src/services/sku/sku.service.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import { SKURepository } from "../../models/sku/sku.repository";
 import { Service, Inject, Container } from "typedi";
-import { TransactionType } from "../../types/sku.type";
+import { Transaction, TransactionType } from "../../types/sku.type";
 
 @Service()
 export class SKUService {
@@ -27,17 +27,24 @@ export class SKUService {
   }
   const {stock:qty} = stock;
 
-  const orderedTransactions = transactions.filter((transaction:{type:TransactionType})=> transaction.type===TransactionType.ORDER) || [];
-  const refundTransactions = transactions.filter((transaction:{type:TransactionType})=> transaction.type===TransactionType.REFUND) ||[];
+  const totalOrderedQuantities = this.getTransactionTotalQuantity(transactions,TransactionType.ORDER );
   
-  const totalOrderedQuantities = orderedTransactions.reduce((accumulator, currentObject) => {
-    return accumulator + currentObject.qty;
-  }, 0);
+  const totalRefundedQuantities =  this.getTransactionTotalQuantity(transactions,TransactionType.REFUND );
   
-  const totalRefundedQuantities = refundTransactions.reduce((accumulator, currentObject) => {
-    return accumulator + currentObject.qty;
-  }, 0);
 
    return qty-totalOrderedQuantities+totalRefundedQuantities;
   };
+
+  /**
+   * method takes transaction and accumulate its quantities
+   * @param transactions 
+   * @param type 
+   * @returns 
+   */
+  private getTransactionTotalQuantity(transactions:Transaction[], type: TransactionType){
+    const filteredTransactions = transactions.filter((transaction:{type:TransactionType})=> transaction.type===type) || [];
+    return filteredTransactions.reduce((accumulator, currentObject) => {
+      return accumulator + currentObject.qty;
+    }, 0);
+  }
 }
